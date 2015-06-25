fb06c336bb48a5fdc34e050e9715d6079d1ad38f
Oleg Tatarchuk <tatarchyk.oleg@gmail.com>
2015-06-25 21:54:19 +0300

    Changed locators

--- src/test/java/ua/oit/selenium2/pages/RozetkaHeaderLoginPopup.java
+++ src/test/java/ua/oit/selenium2/pages/RozetkaHeaderLoginPopup.java
@@ -1,5 +1,6 @@
 package ua.oit.selenium2.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
@@ -9,12 +10,14 @@ import org.openqa.selenium.support.FindBy;
  * Created by Oleg on 02.06.2015.
  */
 public class RozetkaHeaderLoginPopup extends RozetkaHeader{
+    private final String socialBindCloseLinkLocator = "close";
 
-    @FindBy(name = "login")        public WebElement loginField;
-    @FindBy(name = "password")     public WebElement pwdAuthField;
-    @FindBy(name = "auth_submit")  public WebElement loginBtn;
-    @FindBy(xpath = "//div[@class='social-bind social-bind-tiny']/a")
-    public WebElement socialBindCloseLink;
+    @FindBy(name = "login")       @CacheLookup public WebElement loginField;
+    @FindBy(name = "password")    @CacheLookup public WebElement pwdAuthField;
+    @FindBy(name = "auth_submit") @CacheLookup public WebElement loginBtn;
+    @FindBy(name = socialBindCloseLinkLocator) @CacheLookup public WebElement socialBindCloseLink;
+
+    //@FindBy(css = "social-bind-tiny-close novisited") @CacheLookup public WebElement socialBindCloseLink;
 
     public RozetkaHeaderLoginPopup(WebDriver webDriver) {
         super(webDriver);
@@ -23,7 +26,7 @@ public class RozetkaHeaderLoginPopup extends RozetkaHeader{
 
     public void login(String email, String pwd) {
         String signinLinkTextActual = getSigninLinkText();
-        String signinLinkTextExpected = "войдите в личный кабинет";
+        String signinLinkTextExpected = "РІРѕР№РґРёС‚Рµ РІ Р»РёС‡РЅС‹Р№ РєР°Р±РёРЅРµС‚";
 
         boolean isUserNotLoggedIn = signinLinkTextExpected.equals(signinLinkTextActual);
         if (isUserNotLoggedIn) {
@@ -31,9 +34,13 @@ public class RozetkaHeaderLoginPopup extends RozetkaHeader{
             setElementText(email, loginField);
             setElementText(pwd, pwdAuthField);
             clickElement(loginBtn);
-            if (socialBindCloseLink.isDisplayed()) {
-                socialBindCloseLink.click();
-            }
+
+ /*           if (driver.findElements(By.name(socialBindCloseLinkLocator)).size() != 0) {
+                driver.findElement(By.name(socialBindCloseLinkLocator)).click();
+            }*/
+
+
+
         } else {System.out.println("\n *** User is already logged in *** \n");}
     }
 
